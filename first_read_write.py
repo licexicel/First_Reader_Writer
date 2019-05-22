b7be4d90cb121b33d51db99b2191f0282295253d
licexicel <37893175+licexicel@users.noreply.github.com>
2019-05-22 18:31:16 +0800

    Add files via upload

--- first_read_write.py
+++ first_read_write.py
@@ -1,5 +1,14 @@
-# First_Reader_Writer
-# can multi read but one write at the same time 
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
 
 import threading,random,time,sys
 data = 500
@@ -7,8 +16,9 @@ total = 0
 stop = 0
 wthread = []
 rthread = []
-sem = threading.Semaphore()
+#sem = threading.Semaphore()
 readready = threading.Condition(threading.Lock())
+mutexa = threading.Condition(threading.Lock())
 readcount = 0
 rcount = 0
 wcount = 0
@@ -21,30 +31,35 @@ def read(num):
     global total
     global rcount
     global stop
-    while total < 20 and stop == 0:
+    while total < 120 and stop == 0:
             #acquire
-            readready.acquire()
-            try:
-                readcount +=1
-                print("[Reader%d] enter critical section."%(num))
-                print("[Reader%d] read %d"%(num,data))
-                times= random.randint(1,4)
-                total = total + times
-                print("[Reader%d] sleep %d seconds."%(num,times))
-                time.sleep(times)
-            finally:
-                readready.release()
-                rcount +=1
+            mutexa.acquire()
+            readcount +=1
+            rcount+=1
+            if(readcount == 1):
+                readready.acquire()
+            mutexa.release()
+
+            print("    [Reader%d] enter critical section."%(num))
+        
+            print("    [Reader%d] read %d"%(num,data))
+            times= random.randint(1,4)
+            total = total + times
+            print("    [Reader%d] sleep %d seconds."%(num,times))
+            time.sleep(times)
+            print("    [Reader%d] exit critical section."%(num))
+
             #release
-            readready.acquire()
-            try:
-                readcount-=1
-                if not readcount:
-                    readready.notifyAll()
-            finally:
+            mutexa.acquire()
+            readcount-=1
+            if readcount==0:
                 readready.release()
-                print("[Reader%d] exit critical section."%(num))
-    
+            mutexa.release()
+
+            times= random.randint(3,8)
+            total = total + times
+            #print("[Reader%d] sleep %d seconds."%(num,times))
+            time.sleep(times)
     time.sleep(100)
 def write(num):
     global data
@@ -52,11 +67,10 @@ def write(num):
     global total
     global wcount
     global stop
-    while total < 20 and stop == 0:
+    while total < 120 and stop == 0:
         #acquire
         readready.acquire()
-        while readcount > 0:
-            readready.wait()
+        
         print("[Writer%d] enter critical section."%(num))
         data = random.randint(0,99)
         print("[Writer%d] write %d"%(num,data))
@@ -65,10 +79,15 @@ def write(num):
         wcount +=1
         print("[Writer%d] sleep %d seconds."%(num,times))
         time.sleep(times)
+        print("[Writer%d] exit critical section."%(num))
+        
         #release
         readready.release()
-        print("[Writer%d] exit critical section."%(num))
 
+        times= random.randint(1,4)
+        total = total + times
+        #print("[Reader%d] sleep %d seconds."%(num,times))
+        time.sleep(times)
     time.sleep(100)
 for i in range(0,Reader):
     rthread.append(threading.Thread(target = read,args = (i,)))
@@ -79,10 +98,22 @@ for j in range(0,Writer,1):
 
 while 1:
    
-    if total > 20:
+    if total > 120:
         stop = -1
         time.sleep(20)
         print("\nFirst Reader-Writer stop")
         print("Total write counts:%d"%(wcount))
         print("Total read  counts:%d"%(rcount) )
         sys.exit()
+
+
+
+
+        
+
+
+# In[ ]:
+
+
+
+
